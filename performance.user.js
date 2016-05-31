2fdb135e08d8cdf470e3aebe8b0231b94b56f369
Joe <josepha48@gmail.com>
2016-05-31 11:40:21 -0700

    save the performance.user.js file on github
    put here instead of local.  This makes a little sense out of the performance timings object in the new browsers.

---
+++ performance.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name           PerformanceTimings
+// @namespace      http://ldhostmacys.fds.com/
+// @description    Show Performance Timings Of Site
+// @include        http://*.fds.com/*
+// @include        http://*.*.fds.com/*
+// @include        http://*.macys.com/*
+// @include        https://*.macys.com/*
+// @include        http://*.bloomingdales.com/*
+// @include        https://*.bloomingdales.com/*
+// @include        http://ldhostmacys.fds.com:2001/*
+// @include        https://ldhostmacys.fds.com:9443/*
+// @include        https://ldhostmacys.fds.com/*
+// ==/UserScript==
+
+function renderContent() {
+    var j, nmap = performance.timing, start, timeValue;
+    var results = "<b>Performance Timings</b><br>";    
+
+    start = nmap['navigationStart'];
+    
+    for ( j in nmap ) { 
+        timeValue = ( ( nmap[j] <= 0 ) ? 0 : ( nmap[j] - start ) );
+        results += j + " " + timeValue + "<br>";
+    }
+
+    return results;
+}
+
+var buildInfo = document.getElementById("domElementInfo");
+if ( ! buildInfo ) { 
+    buildInfo = document.createElement("div");   
+    buildInfo.id = "domElementInfo";
+    buildInfo.style.background = "white";
+    buildInfo.style.position = "absolute";
+    buildInfo.style.top = "0px";
+    buildInfo.style.left = "0px";
+    buildInfo.style.width = "200px";
+    buildInfo.style.zIndex = 10000;
+    buildInfo.style.overflow = "scroll";
+    buildInfo.style.textAlign = "left";
+}
+
+// call function
+var results = renderContent();
+
+var script = "var bi = document.getElementById('domElementInfo');";
+script += "if (bi) { bi.style.display = 'none'; }"; 
+
+// create script tag for re-rendering the element counts
+var render = document.getElementById("domContentScript");
+if ( ! render ) { 
+    render = document.createElement("script");   
+    render.id = "domContentScript";
+}
+render.innerHTML = renderContent;
+document.body.appendChild(render);
+
+var rerender = "var bi = document.getElementById('domContents');";
+rerender += "if (bi) { bi.innerHTML = renderContent(); }"; 
+
+buildInfo.innerHTML = "<div id='domContents'>" + results + '\n' + '</div><hr><span onclick="' + rerender + '">re-render</span><br><span onclick="' + script + '">close</span>';
+document.body.appendChild(buildInfo);
+
+
