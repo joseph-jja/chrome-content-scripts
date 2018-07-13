1e79738a6d8cadabf514dd57a27420622130d439
Joe Acosta <josepha48@gmail.com>
2018-07-13 13:29:28 -0700

    update options to set and get storage correctly, UI needs love

--- url-block/options.js
+++ url-block/options.js
@@ -25,6 +25,7 @@ function handleAddClick() {
         allowedBlocked = document.getElementById('allow-block-id');
 
     if (!allowBlockUrl.value) {
+        Blocked
         return;
     }
 
@@ -36,10 +37,28 @@ function handleAddClick() {
         // so if we have a parent domain then we can do certain things
         const mapKey = parentDomain.value;
 
-        let alwaysAllow = true;
-        if (allowedBlocked.checked) {
-            alwaysAllow = allowedBlocked.checked;
+        const urlBlockerData = storageItems.urlBlockerData;
+        if (!urlBlockerData) {
+            storageItems.urlBlockerData = {
+                'alwaysAllowed': {},
+                'alwaysBlocked': {}
+            };
         }
+
+        if (allowedBlocked.checked) {
+            let key = storageItems.urlBlockerData.alwaysBlocked[mapKey];
+            if (!key) {
+                storageItems.urlBlockerData.alwaysBlocked[mapKey] = [];
+            }
+            storageItems.urlBlockerData.alwaysBlocked[mapKey].push(abUrl);
+        } else {
+            let key = storageItems.urlBlockerData.alwaysAllowed[mapKey];
+            if (!key) {
+                storageItems.urlBlockerData.alwaysAllowed[mapKey] = [];
+            }
+            storageItems.urlBlockerData.alwaysAllowed[mapKey].push(abUrl);
+        }
+        updateStorage();
     } else {
         const urlBlockerData = storageItems.urlBlockerData;
         if (!urlBlockerData) {
@@ -55,6 +74,22 @@ function handleAddClick() {
 const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', handleAddClick, false);
 
+function renderRow(tr, parts, parent, domainName) {
+    for (let j = 0, jend = parts.length; j < jend; j++) {
+        const td = document.createElement('td');
+        if (parts[j] === 'button') {
+            const button = document.createElement('button');
+            button.innerHTML = 'Delete';
+            button.dataset.domainName = parent + domainName;
+            button.className = 'deleteBlockedDataDomain';
+            td.appendChild(button);
+        } else {
+            td.innerHTML = parts[j];
+        }
+        tr.appendChild(td);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', restore_options => {
     const table = document.getElementById('display-results').tBodies[0];
 
@@ -64,27 +99,45 @@ document.addEventListener('DOMContentLoaded', restore_options => {
             storageItems = items;
 
             const urlBlockerData = storageItems.urlBlockerData
+            if (!urlBlockerData) {
+                return;
+            }
 
-            if (urlBlockerData && urlBlockerData.blocked) {
+            if (urlBlockerData.blocked) {
                 const blockedItems = urlBlockerData.blocked;
                 for (let i = 0, end = blockedItems.length; i < end; i++) {
                     var tr = document.createElement('tr');
                     table.appendChild(tr);
                     const domainName = blockedItems[i];
                     const parts = ['', domainName, 'Blocked', 'button'];
-                    for (let j = 0, jend = parts.length; j < jend; j++) {
-                        const td = document.createElement('td');
-                        if (parts[j] === 'button') {
-                            const button = document.createElement('button');
-                            button.innerHTML = 'Delete';
-                            button.dataset.domainName = domainName;
-                            button.className = 'deleteBlockedDataDomain';
-                            td.appendChild(button);
-                        } else {
-                            td.innerHTML = parts[j];
-                        }
-                        tr.appendChild(td);
-
+                    renderRow(tr, parts, '', doaminName);
+                }
+            }
+            if (urlBlockerData.alwaysBlocked) {
+                const blockedItems = urlBlockerData.alwaysBlocked;
+                const keys = Object.keys(blockedItems);
+                for (let i = 0, end = keys.length; i < end; i++) {
+                    const domains = blockedItems[keys[i]];
+                    for (let m = 0, mend = domains.length; m < mend; m++) {
+                        var tr = document.createElement('tr');
+                        table.appendChild(tr);
+                        const domainName = domains[m];
+                        const parts = [keys[i], domainName, 'Blocked', 'button'];
+                        renderRow(tr, parts, keys[i], domainName);
+                    }
+                }
+            }
+            if (urlBlockerData.alwaysAllowed) {
+                const allowedItems = urlBlockerData.alwaysAllowed;
+                const keys = Object.keys(allowedItems);
+                for (let i = 0, end = keys.length; i < end; i++) {
+                    const domains = allowedItems[keys[i]];
+                    for (let m = 0, mend = domains.length; m < mend; m++) {
+                        var tr = document.createElement('tr');
+                        table.appendChild(tr);
+                        const domainName = domains[m];
+                        const parts = [keys[i], domainName, 'Allowed', 'button'];
+                        renderRow(tr, parts, keys[i], domainName);
                     }
                 }
             }
@@ -94,6 +147,7 @@ document.addEventListener('DOMContentLoaded', restore_options => {
                 if (!domainName) {
                     return;
                 }
+                // FIXME for different storage types
                 const i = storageItems.urlBlockerData.blocked.indexOf(domainName);
                 if (i > -1) {
                     storageItems.urlBlockerData.blocked.splice(i, 1);
