736f18fb2b4a361a7797fb86937373c68b4f43a7
Joe Acosta <josepha48@gmail.com>
2019-08-08 19:09:42 -0700

    hide iframes too!

--- url-block/index.js
+++ url-block/index.js
@@ -1,2 +1,12 @@
 const hostname = window.location.host;
 
+//const workerPath = chrome.runtime.getURL('worker.js');
+//const workerThread = new Worker(workerPath);
+
+setTimeout(() => {
+
+    const iframes = Array.from(document.querySelectorAll('iframe'));
+    iframes.forEach(iframe => {
+        iframe.style.display = 'none';
+    });
+}, 1000);
