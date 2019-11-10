998100b401a3ecde849c73584bbfee28babde561
Joe <josepha48@gmail.com>
2019-11-10 12:59:03 -0800

    todo implement add button

--- url-block/options.js
+++ url-block/options.js
@@ -7,6 +7,30 @@ let storageItems = {
     }
 };
 
+function createTableRow(parentTable, 
+    url, urlCounts, buttonText, buttonClass) {
+    
+    var tr = document.createElement('tr');
+    parentTable.tBodies[0].appendChild(tr);
+                        
+    // domain cell
+    const tddomain = document.createElement('td');
+    tddomain.innerHTML = url;
+    tr.appendChild(tddomain);
+                        
+    const tdcount = document.createElement('td');
+    tdcount.innerHTML = urlCounts;
+    tr.appendChild(tdcount);
+                        
+    const tdbutton = document.createElement('td');
+    const button = document.createElement('button');
+    button.innerHTML = buttonText;
+    button.dataset.domainName = url;
+    button.className = buttonClass;
+    tdbutton.appendChild(button);
+    tr.appendChild(tdbutton);
+}
+
 const port = chrome.extension.connect({
     name: 'Blocked URL Message Channel'
 });
@@ -31,48 +55,11 @@ port.onMessage.addListener(function(msg) {
                 if (msg[tabID] && msg[tabID][tabURL]) {
                     const allowedURLs = msg[tabID][tabURL].allowed;
                     Object.keys(allowedURLs).forEach(url => {
-                        var tr = document.createElement('tr');
-                        tableAllowed.appendChild(tr);
-                        
-                        // domain cell
-                        const tddomain = document.createElement('td');
-                        tddomain.innerHTML = url;
-                        tr.appendChild(tddomain);
-                        
-                        const tdcount = document.createElement('td');
-                        tdcount.innerHTML = allowedURLs[url];
-                        tr.appendChild(tdcount);
-                        
-                        const tddisable = document.createElement('td');
-                        tr.appendChild(tdcount);
-                        const button = document.createElement('button');
-                        button.innerHTML = 'Block';
-                        button.dataset.domainName = url;
-                        button.className = 'blockDomain';
-                        tddisable.appendChild(button);
-                        tr.appendChild(tddisable);
+                        createTableRow(tableAllowed, url, allowedURLs[url], 'Block', 'blockDomain');
                     });
                     const blockedURLs = msg[tabID][tabURL].blocked;
                     Object.keys(blockedURLs).forEach(url => {
-                        var tr = document.createElement('tr');
-                        tableBlocked.appendChild(tr);
-                        
-                        // domain cell
-                        const tddomain = document.createElement('td');
-                        tddomain.innerHTML = url;
-                        tr.appendChild(tddomain);
-                        
-                        const tdcount = document.createElement('td');
-                        tdcount.innerHTML = blockedURLs[url];
-                        tr.appendChild(tdcount);
-                        
-                        const tdenable = document.createElement('td');
-                        const button = document.createElement('button');
-                        button.innerHTML = 'Allow';
-                        button.dataset.domainName = url;
-                        button.className = 'allowDomain';
-                        tdenable.appendChild(button);
-                        tr.appendChild(tdenable);
+                        createTableRow(tableBlocked, url, blockedURLs[url], 'Allow', 'allowDomain');
                     });
                 }
             }
@@ -83,17 +70,92 @@ port.onMessage.addListener(function(msg) {
 });
 port.postMessage('Give me the URLs that have been blocked');
 
+async function getCurrentTab() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.getSelected(tab => {
+            if (tab && tab.url) {
+                resolve(tab.url);    
+            } else {
+                reject();
+            }
+        });
+    });
+}
+                
 function updateStorage() {
     
     chrome.storage.local.set(('urlBlockerData', storageItems),
         function(items) {
-
             if (items) {
                 console.log(items);
             }
-            //window.location.reload();
         });
 }
+
+function handleAllowClick(e) {
+    const tgt = e.target;
+    if (tgt.nodeName.toLowerCase() !== 'button') {
+        return;
+    }    
+    const allowUrl = tgt.dataset.domainName;
+    
+    getCurrentTab().then(furl => {
+        const url = parseHostProtocol(furl).host;
+        if (!storageItems.urlBlockerData.allowed) {
+            storageItems.urlBlockerData.allowed = {};
+        }
+        if (!storageItems.urlBlockerData.allowed[url]) {
+            storageItems.urlBlockerData.allowed[url] = [];
+        }
+
+        const tableAllowed = document.getElementById('allowed-results');
+        createTableRow(tableAllowed, allowUrl, 0, 'Block', 'blockDomain');
+        
+        // delete row from blocked
+        const row = tgt.parentNode.parentNode;
+        const rowParent = row.parentNode;
+        rowParent.removeChild(row);
+
+        storageItems.urlBlockerData.allowed[url].push(allowUrl);
+        updateStorage();
+    }, () => {});
+
+
+}
+const allowButtons = document.getElementById('blocked-results');
+allowButtons.addEventListener('click', handleAllowClick, false);
+
+function handleBlockClick(e) {
+    const tgt = e.target;
+    if (tgt.nodeName.toLowerCase() !== 'button') {
+        return;
+    }    
+    const blockUrl = tgt.dataset.domainName;
+    
+    getCurrentTab().then(furl => {
+        const url = parseHostProtocol(furl).host;
+        if (!storageItems.urlBlockerData.allowed) {
+            storageItems.urlBlockerData.allowed = {};
+        }
+        if (!storageItems.urlBlockerData.allowed[url]) {
+            storageItems.urlBlockerData.allowed[url] = [];
+        }
+
+        const tableBlocked = document.getElementById('blocked-results');
+        createTableRow(tableBlocked, blockUrl, 0, 'Allow', 'allowDomain');
+        
+        // delete row from allowed
+        const row = tgt.parentNode.parentNode;
+        const rowParent = row.parentNode;
+        rowParent.removeChild(row);
+
+        storageItems.urlBlockerData.blocked[url].push(blockUrl);
+        updateStorage();
+    }, () => {});
+}
+const blockButtons = document.getElementById('allowed-results');
+blockButtons.addEventListener('click', handleBlockClick, false);
+
 
 function renderRow(tr, parts, domainName) {
     for (let j = 0, jend = parts.length; j < jend; j++) {
@@ -109,83 +171,9 @@ function renderRow(tr, parts, domainName) {
         }
         tr.appendChild(td);
     }
-}
-
-async function getCurrentTab() {
-    return new Promise((resolve, reject) => {
-        chrome.tabs.getSelected(tab => {
-            if (tab && tab.url) {
-                resolve(tab.url);    
-            } else {
-                reject();
-            }
-        });
-    });
-}
-                       
+}  
         
-function handleEnableClick(e) {
-
-    const tgt = e.target;
-
-    if (tgt.nodeName.toLowerCase() !== 'button') {
-        return;
-    }    
-
-    const abUrl = tgt.dataset.domainName;
-
-    getCurrentTab().then(furl => {
-        const url = parseHostProtocol(furl).host;
-        if (!storageItems.urlBlockerData.allowed) {
-            storageItems.urlBlockerData.allowed = {};
-        }
-        if (!storageItems.urlBlockerData.allowed[url]) {
-            storageItems.urlBlockerData.allowed[url] = [];
-        }
-
-        const table = document.getElementById('enabled-results');
-        var tr = document.createElement('tr');
-        table.appendChild(tr);
-        renderRow(tr, [abUrl, 'button'], abUrl);
-
-        storageItems.urlBlockerData.allowed[url].push(abUrl);
-        updateStorage();
-    }, () => {});
-}
-
-//enabled-results
-const enableButtons = document.getElementById('blocked-results');
-enableButtons.addEventListener('click', handleEnableClick, false);
-
-function handleAddClick(e) {
-    const allowUrl = document.getElementById('allow-url');    
-    
-    getCurrentTab().then(furl => {
-        const url = parseHostProtocol(furl).host;
-        if (!storageItems.urlBlockerData.allowed) {
-            storageItems.urlBlockerData.allowed = {};
-        }
-        if (!storageItems.urlBlockerData.allowed[url]) {
-            storageItems.urlBlockerData.allowed = {};
-            storageItems.urlBlockerData.allowed[url] = [];
-        }
-        
-        const table = document.getElementById('enabled-results');
-        var tr = document.createElement('tr');
-        table.appendChild(tr);
-        renderRow(tr, [allowUrl, 'button'], allowUrl);
-
-        storageItems.urlBlockerData.allowed[url].push(allowUrl);
-        updateStorage();
-    }, () => {});
-}
-
-const addButton = document.getElementById('add-button');
-enableButtons.addEventListener('click', handleAddClick, false);
-
-
 document.addEventListener('DOMContentLoaded', restore_options => {
-    const table = document.getElementById('blocked-results').tBodies[0];
 
     chrome.storage.local.get('urlBlockerData', function(items) {
 
@@ -194,36 +182,39 @@ document.addEventListener('DOMContentLoaded', restore_options => {
 
             if (!items) {
                 storageItems.urlBlockerData = {
-                    blocked: []
+                    allowed: {},
+                    blocked: {}
                 };
                 return;
             }
+            
+            getCurrentTab().then(furl => {
+                const url = parseHostProtocol(furl).host;
 
-            const urlBlockerData = storageItems.urlBlockerData;
-            if (urlBlockerData.blocked) {
-                const blocked = urlBlockerData.blocked;
-                blocked.forEach(domainName => {
-                    var tr = document.createElement('tr');
-                    table.appendChild(tr);
-                    renderRow(tr, [domainName, 'button'], domainName);
-                });
-            }
-            table.addEventListener('click', (e) => {
-                const tgt = e.target;
-                const domainName = tgt.dataset['domainName'];
-                if (!domainName) {
-                    return;
-                }
-                // remove an blocked domain
-                if (storageItems.urlBlockerData.blocked) {
-                    const i = storageItems.urlBlockerData.blocked.indexOf(domainName);
-                    if (i > -1) {
-                        storageItems.urlBlockerData.blocked.splice(i, 1);
-                        updateStorage();
+                const urlBlockerData = storageItems.urlBlockerData;
+
+                const blockedTable = document.getElementById('blocked-results');
+                if (urlBlockerData.blocked) {
+                    const blocked = urlBlockerData.blocked[url];
+                    if (blocked) {
+                        blocked.forEach(domainName => {
+                            createTableRow(blockedTable, 
+                                   domainName, 0, 'Allow', 'allowDomain');
+                        });
                     }
                 }
-            });
 
+                const allowedTable = document.getElementById('allowed-results');
+                if (urlBlockerData.allowed) {
+                    const allowed = urlBlockerData.allowed[url];
+                    if (allowed) {
+                        allowed.forEach(domainName => {
+                            createTableRow(allowedTable, 
+                                   domainName, 0, 'Block', 'blockDomain');
+                        });
+                    }
+                }
+            }, () => {});
         }
     });
 });
