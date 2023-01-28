9f3f809b3f808be4e7f03cb2178979f5d089950c
Joe <josepha48@gmail.com>
2023-01-28 15:57:19 -0800

    revertish

--- aquabid/aquabid.js
+++ aquabid/aquabid.js
@@ -1,6 +1,6 @@
 // search page is slightly different it does not put sortable on table
 const IMAGE_WIDTH = 750;
-setTimeout(() => {
+//setTimeout(() => {
     // change width of all the tables for wider screens
     Array.from(document.querySelectorAll('table')).forEach(tbl => {
         tbl.width = '95%';
@@ -9,7 +9,7 @@ setTimeout(() => {
     Array.from(document.getElementsByTagName('*')).forEach(ele => {
         ele.style.fontSize = '14pt';
     });
-}, 1000);
+//}, 1000);
 
 
 var table = document.querySelector('.sortable tbody');
