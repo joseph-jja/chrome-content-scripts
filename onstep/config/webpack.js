0726e3438143e9edc94d1f27501c830be157c061
Joe A <josepha48@gmail.com>
2025-03-08 15:48:27 -0800

    more code for the electron onstep app

---
+++ onstep/config/webpack.js
@@ -0,0 +1,63 @@
+import path from "path";
+
+const baseDir = process.cwd()
+
+const babelTargets = {
+    "edge": "120",
+    "chrome": "120",
+    "firefox": "120",
+    "safari": "15"
+};
+//const eslintConfig = fs.readFileSync( path.resolve( "./config/eslint.json" ) ).toString();
+
+/*const esJSON = JSON.parse( eslintConfig ),
+    esJSONWP = Object.keys( esJSON.globals );
+
+esJSON.globals = esJSONWP;*/
+
+export default {
+    "mode": "production",
+    "entry": {
+        "renderer": "./js/main"
+    },
+    context: path.resolve(baseDir),
+    //devtool: "cheap-source-map",
+    output: {
+        "path": `${baseDir}/js`,
+        "filename": "renderer.js",
+        "chunkFilename": "renderer.bundle.js",
+        "sourceMapFilename": "renderer.source.map"
+    },
+    optimization: {
+        minimize: false
+    },
+    resolve: {
+        modules: [
+            "node_modules",
+            path.join(baseDir, "js")
+        ],
+        alias: {
+            'js': `${baseDir}/js`
+        },
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: babelTargets,
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: []
+};
