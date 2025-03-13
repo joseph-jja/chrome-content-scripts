0920ef3a6e038112a0ab2de246e9f065cbbf0458
Joe <josepha48@gmail.com>
2025-03-12 18:08:45 -0700

    Update Connection.jsx

--- onstep/js/components/Setup/Connection.jsx
+++ onstep/js/components/Setup/Connection.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 
+import HostConnection from 'js/components/Setup/HostConnection.jsx';
+import COMPortConnection from 'js/components/Setup/COMPortConnection.jsx';
+
+export default function Connection() {
+
+    return (
+        <HostConnection/>
+            { window.electron.operatingSystem() === 'linux' ?
+                <COMPortConnection/> : null }
+    );
+}
