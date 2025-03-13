52a526628f10a8d9d222a71199526b9c21f1aeb9
Joe <josepha48@gmail.com>
2025-03-12 18:05:24 -0700

    Update COMPortConnection.jsx

--- onstep/js/components/Setup/COMPortConnection.jsx
+++ onstep/js/components/Setup/COMPortConnection.jsx
@@ -3,40 +3,61 @@ import React from 'react';
 import CustomInput from 'js/components/base/CustomInput.jsx';
 import CustomButton from 'js/components/base/CustomButton.jsx';
 import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
+import {
+    setupConnection,
+    teardownConnection
+} from 'js/api/request.js';
 
 const { useState } = React;
 
-// TODO figure out how this would work cross platform
-export default function COMPortConnection() {
-    const [comPort, setComPort] = useState(null);
-    const [comPortError, setComPortError] = useState('');
+const SERIAL_PORT_RE = /\/dev\//;
 
-    const setComPortField = (event) => {
-        setComPort(event?.target?.value);
+export default function COMPortConnection() {
+    const [serialPort, setSerialPort] = useState(null);
+    const [serialPortError, setSerialPortError] = useState('');
+
+    const setSerialPortField = (event) => {
+        setSerialPort(event?.target?.value);
     }
 
-    const sendPortConnectCommand = () => {
-        if (comPort && comPort.length > 0) {
-            setComPortError('');
+    const sendConnectCommand = () => {
+        if (serialPort && serialPort.match(SERIAL_PORT_RE)) {
+            setSerialPortError('');
             // now we need to call fetch
             // and send to server
+            setupConnection(serialPort).then(data => {
+                setSerialPortError(data);    
+            }).catch(e => {
+                setSerialPortError(e);
+            });
         } else {
-            setComPortError('Invalid com port entered!');
+            setSerialPortError('Invalid serial port entered!');
         }
     }
     
+    const sendDisconnectCommand = () => {
+        teardownConnection().then(data => {
+            setSerialPortError(data);    
+        }).catch(e => {
+            setSerialPortError(e);
+        });
+    }
+        
     return (
         <>
             <div>
-                <CustomInput type="text" labelText="Enter com port /dev/xxxx"
-                    id="com-port" name="com_port" inputValue={comPort}
-                    onInputChange={setComPortField}/>
-                <ErrorMessage>{comPortError}</ErrorMessage>
+                <CustomInput type="text" labelText="Enter Serial Port (/dev/xxxx)"
+                    id="serial-port" name="serial_port" inputValue={serialPort} size="22"
+                    onInputChange={setSerialPortField}/>
+                <ErrorMessage>{serialPortError}</ErrorMessage>
                 <br/>
-                <CustomButton id="com-port-setup" 
-                    onButtonClick={sendPortConnectCommand}>Connect</CustomButton>
+                <CustomButton id="serial-connect" 
+                    onButtonClick={sendConnectCommand}>Connect</CustomButton>
+                <CustomButton id="serial-disconnect" 
+                    onButtonClick={sendDisconnectCommand}>Disconnect</CustomButton>
             </div>
         </>
-    );      
+    );
 }
+
 
