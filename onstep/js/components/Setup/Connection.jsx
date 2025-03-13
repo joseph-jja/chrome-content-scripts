3794e067fd03e2bcb636c30dc520e731bc4b54a1
Joe <josepha48@gmail.com>
2025-03-12 18:26:59 -0700

    Update Connection.jsx

--- onstep/js/components/Setup/Connection.jsx
+++ onstep/js/components/Setup/Connection.jsx
@@ -62,8 +62,8 @@ export default function Connection() {
             <CustomInput type="text" labelText="Enter Host:Port (xxx.xxx.xxx.xxx:yyyy)"
                     id="host-port" name="host_port" inputValue={hostPort} size="22"
                     onInputChange={setHostPortField}/>
+            { window.electron.operatingSystem() === 'linux' ? <br/> : null }
             { window.electron.operatingSystem() === 'linux' ? 
-                <br/>
                 <CustomInput type="text" labelText="Enter Serial Port (/dev/xxxx)"
                     id="serial-port" name="serial_port" inputValue={serialPort} size="22"
                     onInputChange={setSerialPortField}/>  : null }
