028138bd9798050184ffd64ee0669991502300e2
Joe <josepha48@gmail.com>
2025-03-12 18:18:02 -0700

    Merge connection types

--- onstep/js/components/Setup/Connection.jsx
+++ onstep/js/components/Setup/Connection.jsx
@@ -1,13 +1,73 @@
 import React from 'react';
 
-import HostConnection from 'js/components/Setup/HostConnection.jsx';
-import COMPortConnection from 'js/components/Setup/COMPortConnection.jsx';
+import CustomInput from 'js/components/base/CustomInput.jsx';
+import CustomButton from 'js/components/base/CustomButton.jsx';
+import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
+import {
+    setupConnection,
+    teardownConnection
+} from 'js/api/request.js';
+
+const { useState } = React;
+
+const HOST_PORT_RE = /\d+\.\d+\.\d+\.\d+\:\d*/;
+const SERIAL_PORT_RE = /\/dev\//;
 
 export default function Connection() {
+    const [hostPort, setHostPort] = useState(null);
+    const [serialPort, setSerialPort] = useState(null);
+    const [serialOrHostPortError, setSerialOrHostPortError] = useState('');
 
+    const setSerialPortField = (event) => {
+        setSerialPort(event?.target?.value);
+    }
+    const setHostPortField = (event) => {
+        setHostPort(event?.target?.value);
+    }
+
+    const sendConnectCommand = () => {
+        if (hostPort && hostPort.match(HOST_PORT_RE)) { 
+            setSerialOrHostPortError('');
+            // now we need to call fetch
+            // and send to server
+            setupConnection(hostPort).then(data => {
+                setHostPortError(data);    
+            }).catch(e => {
+                setHostPortError(e);
+            });
+        } else if (serialPort && || serialPort.match(SERIAL_PORT_RE))) {
+            setSerialOrHostPortError('');
+            // now we need to call fetch
+            // and send to server
+            setupConnection(hostPort).then(data => {
+                setHostPortError(data);  
+        } else {
+            setSerialOrHostPortError('Invalid host and port or COM / tty port entered!');
+        }
+    }
+    
+    const sendDisconnectCommand = () => {
+        teardownConnection().then(data => {
+            setHostPortError(data);    
+        }).catch(e => {
+            setHostPortError(e);
+        });
+    }
+    
     return (
-        <HostConnection/>
+        <div>
+            <CustomInput type="text" labelText="Enter Host:Port (xxx.xxx.xxx.xxx:yyyy)"
+                    id="host-port" name="host_port" inputValue={hostPort} size="22"
+                    onInputChange={setHostPortField}/>
             { window.electron.operatingSystem() === 'linux' ?
-                <COMPortConnection/> : null }
+                <CustomInput type="text" labelText="Enter Serial Port (/dev/xxxx)"
+                    id="serial-port" name="serial_port" inputValue={serialPort} size="22"
+                    onInputChange={setSerialPortField}/> : null }
+            <ErrorMessage>{serialOrHostPortError}</ErrorMessage>
+            <br/>
+            <CustomButton id="serial-connect" 
+                onButtonClick={sendConnectCommand}>Connect</CustomButton>
+            <CustomButton id="serial-disconnect" 
+                onButtonClick={sendDisconnectCommand}>Disconnect</CustomButton>
     );
 }
