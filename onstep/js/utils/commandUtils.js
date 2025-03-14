3f445f1f21db0f5a847c801133b2c7e6a943b891
Joe <josepha48@gmail.com>
2025-03-13 18:10:01 -0700

    return all error messages

--- onstep/js/utils/commandUtils.js
+++ onstep/js/utils/commandUtils.js
@@ -9,26 +9,15 @@ export async function daisyChainBooleanCommands(commands = []) {
     let i = 0, 
         end = commands.length;
     let success = true;
-    let response = 'Success';
+    const response = [];
     while (i < end && success) {
         const cmd = commands[i];        
         const [err, result] = await PromiseWrapper(sendCommand(cmd));
         if (err) {
             success = false;
-            response = err;
-        } else if (result !== 0) {
-            if (result.startsWith('Command response:')) {
-                const msg = result.replace('Command response:', '').trim();
-                if (+msg === 1) {
-                    success = false;
-                    response = `Error sending command, got: ${result}`;
-                } else {
-                    response += ' ' + msg?.trim()?.replace(/\#/g, '');
-                }
-            } else {
-                success = false;
-                response = `Error sending command, got: ${result}`;
-            }
+            response.push(err);
+        } else {
+            response.push(result);
         }
         i++;
     }
