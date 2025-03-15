8c003368b1e2c6b00df547a652aab34b6aaeb1c6
Joe <josepha48@gmail.com>
2025-03-15 12:08:17 -0700

    Add checkbox for has response

--- onstep/js/components/Advanced/CustomCommand.jsx
+++ onstep/js/components/Advanced/CustomCommand.jsx
@@ -5,25 +5,31 @@ import CustomInput from 'js/components/base/CustomInput.jsx';
 import CustomButton from 'js/components/base/CustomButton.jsx';
 import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
 import {
-    daisyChainBooleanCommands
-} from 'js/utils/commandUtils.js';
+    sendCommand
+} from 'js/api/request.js';
+import PromiseWrapper from 'js/utils/PromiseWrapper.js';
 
 const { useState } = React;
 
 export default function CustomCommand() {
     const [commandField, setCommandField] = useState(null);
+    const [responseField, setResponseField] = useState(null);
     const [commandFieldError, setCommandFieldError] = useState(null);
         
     const setCommandFromForm = (event) => {
         const value = event?.target?.value;
         setCommandField(value);
     }
+    const setResponseFieldForm = (event) => {
+        const value = event?.target?.value;
+        setResponseField(value);
+    }
     
     const sendCommandToServer = async () => {
         if (commandField) {
             setCommandFieldError('');
-            const results = await daisyChainBooleanCommands([commandField]);
-            setCommandFieldError(results);
+            const [err, results] = await PromiseWrapper(sendCommand(commandField, false));
+            setCommandFieldError(err || results);
         } else {
             setCommandFieldError('Invalid command entered!');
         }
@@ -34,6 +40,9 @@ export default function CustomCommand() {
             <CustomInput type="text" labelText="Enter command (:xxxx#)" size="12"
                 id="command-field" name="command_field" inputValue={commandField}
                 onInputChange={setCommandFromForm}/>
+            <CustomInput type="checkbox" labelText="Has response" 
+                id="response-field" name="response_field" inputValue={responseField}
+                onInputChange={setResponseFieldForm}/>
             <ErrorMessage>{commandFieldError}</ErrorMessage>
             <br/>
             <CustomButton id="command-button" 
