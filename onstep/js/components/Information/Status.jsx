77086ea77d2dcf4bf95340c2a7ff5ce2be797bcf
Joe <josepha48@gmail.com>
2025-03-15 12:03:42 -0700

    Remove unused import

--- onstep/js/components/Information/Status.jsx
+++ onstep/js/components/Information/Status.jsx
@@ -4,9 +4,6 @@ import Container from 'js/components/base/Container.jsx';
 import CustomInput from 'js/components/base/CustomInput.jsx';
 import CustomButton from 'js/components/base/CustomButton.jsx';
 import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
-import {
-    daisyChainBooleanCommands
-} from 'js/utils/commandUtils.js';
 import StorageBox from "js/storage/StorageBox.js";
 
 const {
