3a156a970330422d6d13a88725ee4c1c3b196d83
Joe <josepha48@gmail.com>
2025-03-15 11:32:19 -0700

    Create generate command object.js

---
+++ onstep/js/utils/generate command object.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs';
+import readline from 'node:readline';
+import process from 'node:process';
+
+const basedir = process.cwd();
+
+const instream = fs.createReadStream(`${basedir}/documentation/commands.txt`);
+
+const rl = readline.createInterface({
+    input: instream
+});
+
+const commandOptions = {};
+
+const MATCH_COMMAND = /\:[A-Za-z0-9\/\:\*\$\.\+\-\!\,\/\?\[\]]*\#/g;
+
+rl.on('line', line => {
+
+    const parts = line.match(MATCH_COMMAND);
+    console.log(parts);
+    if (!parts || parts.length > 2 || parts.length < 1) {
+        return;
+    }
+    const middle = parts[0];
+    const [description, reply] = line.split(middle); 
+    commandOptions[parts[0].trim()] = {
+        description: description.trim(), 
+        reply: reply.trim()
+    };
+});
+
+rl.on('close', () => {
+    fs.writeFileSync(`${basedir}/documentation/commands.json`, JSON.stringify((commandOptions));
+    console.log("Commands processed: ", Object.keys(commandOptions).length);
+});
