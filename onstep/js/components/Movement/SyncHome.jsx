759237a43eaf2091cb3643bf0ef52e38ad78c42a
Joe <josepha48@gmail.com>
2025-03-16 09:06:12 -0700

    Add buttons for sync and home

--- onstep/js/components/Movement/SyncHome.jsx
+++ onstep/js/components/Movement/SyncHome.jsx
@@ -4,65 +4,44 @@ import Container from 'js/components/base/Container.jsx';
 import CustomButton from 'js/components/base/CustomButton.jsx';
 import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
 import {
-    daisyChainBooleanCommands
-} from 'js/utils/commandUtils.js';
-import StorageBox from "js/storage/StorageBox.js";
+    sendCommand
+} from 'js/api/request.js';
+import PromiseWrapper from 'js/utils/PromiseWrapper.js';
 
 const { useState } = React;
 
 // TODO figure out how this would work cross platform
-export default function Rates() {
-    const [trackingRate, setTrackingRate] = useState(null);
-    const [trackingRateError, setTrackingRateError] = useState(null);
+export default function SyncHome() {
+    const [homeSync, setHomeSync] = useState(null);
+    const [homeSyncError, setHomeSyncError] = useState(null);
 
-    const setTrackingRateValue = async (event) => {
+    const setSyncHomeValue = async (event) => {
         const targetObj = event?.target?.id;
         let cmd;
-        if (targetObj === 'tracking-increase') {
-            cmd = ':T+#';
-            setTrackingRate(targetObj);
-        } else if (targetObj === 'tracking-reset') {
-            cmd = ':TR#';
-            setTrackingRate(targetObj);
-        } else if (targetObj === 'tracking-decrease') {
-            cmd = ':T-#';
-            setTrackingRate(targetObj);
-        } else if (targetObj === 'tracking-solar') {
-            cmd = ':TS#';
-            setTrackingRate(targetObj);
-        } else if (targetObj === 'tracking-lunar') {
-            cmd = ':TL#';
-            setTrackingRate(targetObj);
-        } else if (targetObj === 'tracking-default') {
-            cmd = ':TQ#';
-            setTrackingRate(targetObj);
+        if (targetObj === 'go-home') {
+            cmd = ':hC#';
+            setHome(targetObj);
+        } else if (targetObj === 'sync-position') {
+            cmd = ':CM#';
+            setHome(targetObj);
         }
         if (cmd) {
-            const [err, results] = await daisyChainBooleanCommands([cmd, ':GT#']);
+            const [err, results] = await PromiseWrapper(sendCommand(cmd, false));
             if (err || results !== 0) {
-                setTrackingRateError(err || results);
+                setHomeSync(err || results);
             } else {
-                setTrackingRateError('');
+                setHomeSync('');
             }
         }
     };
 
     return ( 
         <Container class="wrapper">
-            <CustomButton id="tracking-increase" 
-                onButtonClick={setTrackingRateValue}>Increase Tracking</CustomButton>
-            <CustomButton id="tracking-reset" 
-                onButtonClick={setTrackingRateValue}>Reset Tracking</CustomButton>
-            <CustomButton id="tracking-decrease" 
-                onButtonClick={setTrackingRateValue}>Decrease Tracking</CustomButton>
-            <br/>
-            <CustomButton id="tracking-default" 
-                onButtonClick={setTrackingRateValue}>Tracking Default Rate</CustomButton>
-            <CustomButton id="tracking-solar" 
-                onButtonClick={setTrackingRateValue}>Tracking Solar Rate</CustomButton>
-            <CustomButton id="tracking-lunar" 
-                onButtonClick={setTrackingRateValue}>Tracking Lunar Rate</CustomButton>
-            <ErrorMessage>{trackingRateError}</ErrorMessage>                
+            <CustomButton id="go-home" 
+                onButtonClick={setSyncHomeValue}>Go Home Tracking</CustomButton>
+            <CustomButton id="sync-position" 
+                onButtonClick={setTrackingRateValue}>Sync Position</CustomButton>
+            <ErrorMessage>{setSyncHomeValue}</ErrorMessage>                
         </Container>
     );
 }
