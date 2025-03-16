8b2015447fd679262e7b634df6e045dc7d3818b8
Joe <josepha48@gmail.com>
2025-03-15 18:10:06 -0700

    Fix retrieving the data from server

--- onstep/js/tabs/Advanced.jsx
+++ onstep/js/tabs/Advanced.jsx
@@ -7,6 +7,10 @@ import {
     getCommandList
 } from 'js/api/request.js';
 import StorageBox from "js/storage/StorageBox.js";
+import { 
+    safeParse,
+    safeStringify
+} from 'js/utils/jsonUtils.js';
 
 const {
     useSyncExternalStore,
@@ -16,38 +20,50 @@ const {
 
 const ONSTEP_OPTIONS_LIST_NAME = 'onstep_options_list';
 
-export default function Tracking() {
+function useCommandList() {
     const storageBox = useSyncExternalStore(StorageBox.subscribe, StorageBox.getSnapshot);
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        const apiData = storageBox.get(ONSTEP_OPTIONS_LIST_NAME);
+        if (!apiData) {
+            getCommandList().then(results => {
+                const optionData = safeParse(results);
+                const formattedData = Object.keys(optionData).map(key => {
+                    return {
+                        key: key,
+                        data: optionData[key]
+                    };
+                });
+                setData(formattedData);
+                storageBox.set(ONSTEP_OPTIONS_LIST_NAME, safeStringify(formattedData));
+            }).catch(e => console.error(e));
+        } else {
+                setData(safeParse(apiData));
+        }
+    }, []);
+    return data;
+}
+
+
+export default function Tracking() {
     const [options, setOptions] = useState(null);
+    const data = useCommandList(); 
     
     const selectionFieldChange = (event) => {
     
     
     }
     
-    useEffect(() => {
-        let apiData = StorageBox.getItem(ONSTEP_OPTIONS_LIST_NAME);
-        if (!apiData) {
-            getCommandList().then(results => {
-                try {
-                    apiData = JSON.parse(results);
-                    //StorageBox.setItem(ONSTEP_OPTIONS_LIST_NAME, apiData);
-                } catch(err) {
-                    console.error(err);
-                }
-            }).catch(e => {
-                  console.error(e);
-            });
-        }
-        return () => {};
-    }, []);
-
     return (
         <>
             <CustomCommand/>
             <CustomSelect id="all-options" name="all_options"
                 labelText="All Commands" size="10"
-                onSelectChange={selectionFieldChange}></CustomSelect>
+                onSelectChange={selectionFieldChange}>
+                {
+                    
+                }
+                </CustomSelect>
         </>
     );
 }
