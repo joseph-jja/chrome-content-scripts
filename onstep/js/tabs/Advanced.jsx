c3064d37df8ddb122cd1a3a0e7ddc4988622066a
Joe <josepha48@gmail.com>
2025-03-22 10:21:46 -0700

    Use new component

--- onstep/js/tabs/Advanced.jsx
+++ onstep/js/tabs/Advanced.jsx
@@ -1,75 +1,16 @@
 import React from 'react';
 
 import Container from 'js/components/base/Container.jsx';
-import CustomCommand from 'js/components/Advanced/CustomCommand.jsx';
-import CustomSelect from 'js/components/base/CustomSelect.jsx';
-import CustomOption from 'js/components/base/CustomOption.jsx';
-import {
-    getCommandList
-} from 'js/api/request.js';
-import StorageBox from "js/storage/StorageBox.js";
-import { 
-    safeParse,
-    safeStringify
-} from 'js/utils/jsonUtils.js';
-
-const {
-    useSyncExternalStore,
-    useEffect,
-    useState
-} = React;
-
-const ONSTEP_OPTIONS_LIST_NAME = 'onstep_options_list';
-
-function useCommandList() {
-    const storageBox = useSyncExternalStore(StorageBox.subscribe, StorageBox.getSnapshot);
-    const [data, setData] = useState(null);
-    useEffect(() => {
-        const apiData = storageBox.get(ONSTEP_OPTIONS_LIST_NAME);
-        if (!apiData) {
-            getCommandList().then(results => {
-                const optionData = safeParse(results);
-                const formattedData = Object.keys(optionData).map(key => {
-                    return {
-                        key: key,
-                        data: optionData[key]
-                    };
-                });
-                setData(formattedData);
-                storageBox.set(ONSTEP_OPTIONS_LIST_NAME, safeStringify(formattedData));
-            }).catch(e => console.error(e));
-        } else {
-                setData(safeParse(apiData));
-        }
-    }, []);
-    return data;
-}
-
+import CustomCommand from 'js/components/Advanced/CustomCommand
+import AllCommandsList from 'js/components/Advanced/AllCommandsList.jsx';
 
 export default function Tracking() {
-    const [options, setOptions] = useState(null);
-    const data = useCommandList(); 
-    
-    const selectionFieldChange = (event) => {
-    
-    
-    }
     
     return (
         <Container>
             <CustomCommand/>
             <hr/>
-            <CustomSelect id="all-options" name="all_options"
-                labelText="All Commands" size="10"
-                onSelectChange={selectionFieldChange}>
-                {data?.map((item) => (
-                    <CustomOption value={item?.key}>
-                        {item?.data?.description}&nbsp;&nbsp;
-                        {item?.key}&nbsp;&nbsp;
-                        {item?.data?.reply}
-                    </CustomOption>
-                ))}
-            </CustomSelect>
+            <AllCommandsList/>
         </Container>
     );
 }
