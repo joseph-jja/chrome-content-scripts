294bed48b3c1b189fdf270b776dd5887e0229412
Joe <josepha48@gmail.com>
2025-03-22 10:11:36 -0700

    add container

--- onstep/js/tabs/Tracking.jsx
+++ onstep/js/tabs/Tracking.jsx
@@ -6,10 +6,10 @@ import Rates from 'js/components/Tracking/Rates.jsx';
 export default function Tracking() {
 
     return (
-        <>
+        <Container>
             <ToggleTracking/>
             <hr/>
             <Rates/>
-        </>
+        </Container>
     );
 }
