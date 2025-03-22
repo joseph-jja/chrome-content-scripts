e766d1354bb27c35e26473415ea4ad46644b2913
Joe <josepha48@gmail.com>
2025-03-22 10:13:32 -0700

    add import

--- onstep/js/tabs/Setup.jsx
+++ onstep/js/tabs/Setup.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Connection from 'js/components/Setup/Connection.jsx';
 import Location from 'js/components/Setup/Location.jsx';
 import DateTime from 'js/components/Setup/DateTime.jsx';
+import Container from 'js/components/base/Container.jsx';
 
 // composite of components for the page
 export default function Setup() {
