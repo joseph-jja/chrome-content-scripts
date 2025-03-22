5db616be48df052205063091a5e7c242016238af
Joe <josepha48@gmail.com>
2025-03-22 10:22:20 -0700

    bug fix

--- onstep/js/tabs/Advanced.jsx
+++ onstep/js/tabs/Advanced.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Container from 'js/components/base/Container.jsx';
-import CustomCommand from 'js/components/Advanced/CustomCommand
+import CustomCommand from 'js/components/Advanced/CustomCommand.jsx';
 import AllCommandsList from 'js/components/Advanced/AllCommandsList.jsx';
 
 export default function Tracking() {
