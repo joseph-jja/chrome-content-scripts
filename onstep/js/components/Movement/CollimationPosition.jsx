5dc0303308c4fb3eb8904bb7c6bc42f8ca46a005
Joe <josepha48@gmail.com>
2025-03-22 11:19:37 -0700

    To do test this

--- onstep/js/components/Movement/CollimationPosition.jsx
+++ onstep/js/components/Movement/CollimationPosition.jsx
@@ -4,14 +4,13 @@ import Container from 'js/components/base/Container.jsx';
 import CustomButton from 'js/components/base/CustomButton.jsx';
 import ErrorMessage from 'js/components/base/ErrorMessage.jsx';
 import {
-    sendCommand
-} from 'js/api/request.js';
-import PromiseWrapper from 'js/utils/PromiseWrapper.js';
+    daisyChainBooleanCommands
+} from 'js/utils/commandUtils.js';
+import StorageBox from "js/storage/StorageBox.js";
 
 const { useState } = React;
 
 const DOBSONIAN_COLLIMATION_POSITION = '//:Sas10:00:00#';
-const GOTO_HORIZONTAL_POSITION = ':MA#';
 
 // TODO figure out how this would work cross platform
 export default function CollimationPosition() {
@@ -19,13 +18,8 @@ export default function CollimationPosition() {
     const [collimateError, setCollimateError] = useState(null);
 
     const setCollimationPosition = async (event) => {
-        const targetObj = event?.target?.id;
-        const [err, results] = await PromiseWrapper(sendCommand(DOBSONIAN_COLLIMATION_POSITION, true));
-        if (err || results !== 0) {
-            setCollimateError(err || results);
-        } else {
-            setCollimateError('');
-        }
+        const results = await daisyChainBooleanCommands([DOBSONIAN_COLLIMATION_POSITION, ':MA#', ':GA#']);
+        setCollimateError(results);
     };
 
     return ( 
