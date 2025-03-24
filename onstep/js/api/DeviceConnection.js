09d86aa6fe7fb9ec803802d0e1e48cb80351693f
Joe <josepha48@gmail.com>
2025-03-23 18:21:40 -0700

    Create DeviceConnection.js

---
+++ onstep/js/api/DeviceConnection.js
@@ -0,0 +1,27 @@
+import {
+    EventEmitter
+} from 'node:events';
+
+export default class DeviceConnection extends EventEmitter {
+
+    constructor() {
+        super();
+        this.isConnected = false;
+    }
+
+    isConnected() {
+        return this.isConnected;
+    }
+
+    connect() {
+        throw new Error('Child must implement');
+    }
+
+    disconnect() {
+        throw new Error('Child must implement');
+    }
+
+    sendCommand(command, returnsData = true) {
+        throw new Error('Child must implement');
+    }
+}
