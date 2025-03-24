c33b5ff0659a1153290e3ff9459a8e2e86ca00a4
Joe <josepha48@gmail.com>
2025-03-23 18:35:31 -0700

    Update SerialPort.js

--- onstep/js/api/SerialPort.js
+++ onstep/js/api/SerialPort.js
@@ -1,15 +1,12 @@
 import fs from 'node:fs/promises';
-import {
-    EventEmitter
-} from 'node:events';
 
-export default class SerialPort extends EventEmitter {
+import DeviceConnection from '#server/api/DeviceConnection.js';
+
+export default class SerialPort extend DeviceConnection {
 
     constructor() {
         super();
-        this.socket = undefined;
         this.data = [];
-        this.isConnected = false;
     }
 
     connect(ttyDevice) {
@@ -19,7 +16,7 @@ export default class SerialPort extends EventEmitter {
             }
 
             fs.open(ttyDevice, 'r+').then(fd => {
-                this.socket = fd;
+                this.device = fd;
                 this.isConnected = true;
                 return resolve('Success');
             }).catch(e => {
@@ -32,20 +29,20 @@ export default class SerialPort extends EventEmitter {
     sendCommand(command, returnsData = true) {
         return new Promise((resolve, reject) => {
             this.data = [];
-            if (!this.socket && !this.isConnected) {
+            if (!this.device && !this.isConnected) {
                 return reject('Not connected!');
             }
 
-            this.socket.write(command).then(async res => {
+            this.device.write(command).then(async res => {
                 if (!returnsData) {
                     return resolve('no reply');
                 }
-                const data = await this.socket.read();
+                const data = await this.device.read();
                 const buffer = new Int8Array(data?.buffer);
                 let result = '',
                     i = 0,
                     end = buffer?.length || 0;
-                while (!result.includes('#') && i < end) {
+                while (i < end) {
                     const charData = String.fromCharCode(buffer[i]);
                     const charCode = charData.charCodeAt(0); 
                     if (charCode > 32 && charCode < 127) {
@@ -65,10 +62,10 @@ export default class SerialPort extends EventEmitter {
 
     disconnect() {
         return new Promise((resolve, reject) => {
-            if (!this.socket) {
+            if (!this.device) {
                 return reject('Not connected!');
             }
-            this.socket.close();
+            this.device.close();
             this.isConnected = false;
             resolve('Closed');
         });
