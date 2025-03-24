30d729fdf51f3eb35c352eec240eb37dc7ac6dcf
Joe A <josepha48@gmail.com>
2025-03-24 12:54:41 -0700

    update test

--- onstep/tests/api/socketConnectionTest.js
+++ onstep/tests/api/socketConnectionTest.js
@@ -8,14 +8,37 @@ const Connection = new SocketConnection();
 const host = '192.168.0.1',
     port = 9999;
     
-Connection.connect(host, port).then(async resp => {
-    console.log('Success to connect');
+Connection.connect({ host: host, port: port }).then(async resp => {
+    //console.log('Success to connect', resp, Connection.usbPort, Connection.isConnected); 
+    console.log('Success ', resp, Connection.isConnected());
+    if (Connection.isConnected()) {
+        console.log('We have a usb port and fd', Connection.device);
+    }      
     
-    const res = await Connection.sendCommand('GET /\r\n\r\n');
-    console.log(res);
+    try {
+        const res = await Connection.sendCommand(':GC#');
+        console.log(res);
+    } catch(e) {
+        console.log('Command error', e);
+    }
+    
+    try {
+        const res = await Connection.sendCommand(':Qe#', false);
+        console.log(res);
+    } catch(e) {
+        console.log('Command error', e);
+    }
+
+    try {
+        const res = await Connection.sendCommand(':Ga#');
+        console.log(res);
+    } catch(e) {
+        console.log('Command error', e);
+    }
     
     await Connection.disconnect();
 }).catch(e => {        
     console.log('Failed to connect');
 });
+
 
