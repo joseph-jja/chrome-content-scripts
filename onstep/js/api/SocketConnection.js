36d4e8192ab3155989c5ff15ef44474cbe30eba2
Joe <josepha48@gmail.com>
2025-03-23 18:29:43 -0700

    Update SocketConnection.js

--- onstep/js/api/SocketConnection.js
+++ onstep/js/api/SocketConnection.js
@@ -8,7 +8,7 @@ export default class SocketConnection extends DeviceConnection {
 
     constructor() {
         super();
-        this.client = new Socket();
+        this.device = new Socket();
         this.data = [];
         this.isConnected = false;
     }
@@ -19,16 +19,16 @@ export default class SocketConnection extends DeviceConnection {
                 return reject('Invalid host and or port!');
             }
 
-            this.client.connect(port, host, () => {
+            this.device.connect(port, host, () => {
                 this.isConnected = true;
                 return resolve('Success');
             });
 
-            this.client.on('error', (err) => {
+            this.device.on('error', (err) => {
                 return reject(err);
             });
 
-            this.client.on('data', msg => {
+            this.device.on('data', msg => {
                 const results = msg.toString()
                 this.data.push(results);
                 this.emit('readEnd');
@@ -42,14 +42,14 @@ export default class SocketConnection extends DeviceConnection {
     sendCommand(command, returnsData = true) {
         return new Promise((resolve, reject) => {
             this.data = [];
-            if (!this.client) {
+            if (!this.device) {
                 return reject('Not connected!');
             }
 
             this.once('readEnd', () => {
                 return resolve(this.data.join(''));
             });
-            this.client.write(command);
+            this.device.write(command);
             if (!returnsData) {
                 this.removeAllListeners('readEnd');
                 return resolve('no reply');
@@ -59,12 +59,12 @@ export default class SocketConnection extends DeviceConnection {
 
     disconnect() {
         return new Promise((resolve, reject) => {
-            if (!this.client && isConnected()) {
+            if (!this.device && isConnected()) {
                 return reject('Not connected!');
             }
-            this.client.removeAllListeners('error');
-            this.client.removeAllListeners('data');
-            this.client.end();
+            this.device.removeAllListeners('error');
+            this.device.removeAllListeners('data');
+            this.device.end();
             this.isConnected = false;
             return resolve('Closed');
         });
