ff45172fc0d955281bb21c05c59a11fde5a6c3da
josepha48 <josepha48@gmail.com>
2025-03-23 21:11:01 -0700

    add list of commands that have no reply, to remove this boolean from ui

---
+++ onstep/js/data/noReplayCommands.js
@@ -0,0 +1,19 @@
+
+
+const COMMANDS_WITH_NO_REPLY = [
+  ':Wn#',   ':Q#',    ':Me#',      ':Mw#',
+  ':Mn#',   ':Ms#',   ':Qe#',      ':Qw#',
+  ':Qn#',   ':Qs#',   ':Mgdnnnn#', ':RG#',
+  ':RC#',   ':RM#',   ':RS#',      ':Rn#',
+  ':TQ#',   ':TR#',   ':T+#',      ':T-#',
+  ':TS#',   ':TL#',   ':TK#',      ':CS#',
+  ':LB#',   ':LN#',   ':LCnnnn#',  ':LD#',
+  ':LL#',   ':L!#',   ':$QZ+#',    ':$QZ-#',
+  ':$QZZ#', ':$QZ/#', ':$QZ!#',    ':hF#',
+  ':hC#',   ':FQ#',   ':FF#',      ':FS#',
+  ':F[n]#', ':F+#',   ':F-#',      ':FR[sn]#',
+  ':FZ#',   ':FH#',   ':Fh#',      ':B+#',
+  ':B-#',   ':U#'
+];
+
+export default COMMANDS_WITH_NO_REPLY;
---
+++ onstep/js/utils/commandParser.js
@@ -0,0 +1,36 @@
+import fs from 'node:fs/promises';
+import process from 'node:process';
+
+import {
+    safeParse
+} from '#server/utils/jsonUtils.js';
+    
+const NO_REPLY = 'Reply: [none]';
+
+const basedir = process.cwd();
+
+const COMMAND_FILE = `${basedir}/js/data/commands.json`;
+
+export async function loadCommandDataFile() {
+
+    const commandFile = await fs.readFile(COMMAND_FILE);
+    const commands = safeParse(commandFile.toString());
+
+    const commandsWithNoReply = Object.keys(commands).filter(key => {
+        const item = commands[key];
+        return (item.reply === NO_REPLY); 
+    });
+    
+    /*.map(key => {
+        const item = commands[key];
+        return item; 
+    });
+    console.log(commandsWithNoReply);*/
+    return commandsWithNoReply;
+}
+
+loadCommandDataFile();
+
+
+
+
