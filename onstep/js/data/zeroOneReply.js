807fb473f4e5954f6617e4b43156c117a6cd495e
Joe A <josepha48@gmail.com>
2025-09-17 16:17:56 -0700

    update commands with no reply

---
+++ onstep/js/data/zeroOneReply.js
@@ -0,0 +1,32 @@
+
+const COMMANDS_WITH_ZERO_OR_ONE_REPLY = [
+  ':Te#',
+  ':Td#',
+  ':Tr#',
+  ':Tn#',
+  ':Lo',
+  ':LW',
+  ':$BR',
+  ':$BD',
+  ':WR',
+  ':AW#',
+  ':A1#',
+  ':A+#',
+  ':hQ#',
+  ':hP#',
+  ':hR#',
+  ':FA#',
+  ':fA#',
+  ':FA',
+  ':Fa#',
+  ':FB',
+  ':FC',
+  ':Fc#',
+  ':Fc',
+  ':FD',
+  ':FP',
+  ':FS'
+ ];
+
+
+export default COMMANDS_WITH_ZERO_OR_ONE_REPLY;
