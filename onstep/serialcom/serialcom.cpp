83d95adcf60658bb7d47955ff743a8c076d2d6ae
Joe A <josepha48@gmail.com>
2025-09-29 14:45:20 -0700

    try to migrate to actually opening files

--- onstep/serialcom/serialcom.cpp
+++ onstep/serialcom/serialcom.cpp
@@ -187,7 +187,7 @@ Napi::Value Read(const Napi::CallbackInfo& info) {
 }
 
 // void write(string data)
-Napi::Number Write(const Napi::CallbackInfo& info) {
+Napi::value Write(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     // 1. Check arguments
