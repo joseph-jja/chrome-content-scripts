a3796f23108e23ce235e6cc234d2cea215bd5388
Joe A <josepha48@gmail.com>
2026-04-20 14:38:45 -0700

    initial plan to split code out of server to routes

---
+++ onstep/js/routes/connection.js
@@ -0,0 +1,54 @@
+import SerialPort from '#server/api/SerialPort.js';
+import SocketConnection from '#server/api/SocketConnection.js';
+
+let connection;
+
+export async function connect(options) {
+
+    if (connection) {
+        return connection;
+    }
+
+    if (options.host && options.port) {
+        connection = new SocketConnection();
+        return connection.connect({
+            host: host,
+            port: port
+        });
+    } else if (options.device) {
+        connection = new SerialPort();
+        return connection.connect({
+            usbDevice: commandOption
+        });
+    }
+    return Promise.reject('Invalid connection options');
+}
+
+export async function sendCommand(command, isBoolean, hasResponse, terminatorCharacter, maxReadLength) {
+
+    if (connection?.isConnected() && command) {
+
+        if (command.startsWith(':') && command.endsWith('#')) {
+
+            const terminatorChar = terminatorCharacter ?
+                decodeURIComponent(terminatorCharacter) : undefined;
+
+            const forceResponseResponse = hasResponse ||
+                isBoolean || terminatorChar ||
+                Number.isInteger(maxReadLength);
+
+            console.log('Should be returning data? ', forceResponseResponse, 'boolean? ', isBoolean, 'Termination character? ', terminatorChar, 'Maximum read length? ', maxReadLength);
+
+            return connection.sendRecieveCommand(command, hasResponse,
+                isBoolean, terminatorChar, maxReadLength)
+        }
+        return Promise.reject('Invalid command');
+    }
+
+    export function disconnect() {
+
+        if (connection?.isConnected()) {
+            connection.disconnect();
+            connection = null;
+        }
+    }
---
+++ onstep/js/routes/getStarView.js
@@ -0,0 +1,49 @@
+export default async function getStarView(apiEndpoint, authToken, latitude, longitude, ra, dec) {
+
+    if (!authToken) {
+        return Promise.reject('No configuration found for astronomy api');
+    }
+
+    const latitude = req.query?.latitude;
+    const longitude = req.query?.longitude;
+    const ra = req.query?.ra;
+    const dec = req.query?.dec;
+    if (!ra || !dec || !latitude || !longitude) {
+        return Promise.reject('No right ascension or declination or missing latitude or longitude!');
+    }
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const day = `${now.getDate()}`.padStart(2, '0');
+    const yyyymmdd = `${year}-${month}-${day}`;
+    const payload = {
+        "style": "navy",
+        "observer": {
+            "latitude": parseFloat(latitude),
+            "longitude": parseFloat(longitude),
+            "date": yyyymmdd
+        },
+        "view": {
+            "type": "area",
+            "parameters": {
+                "position": {
+                    "equatorial": {
+                        "rightAscension": parseFloat(ra),
+                        "declination": parseFloat(dec)
+                    }
+                },
+                "zoom": 1
+            }
+        }
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${authToken}`
+        },
+        body: safeStringify(payload)
+    };
+
+    return fetch(`${apiEndpoint}/api/v2/studio/star-chart`, options);
+}
