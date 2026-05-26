cc457fa6b9a172c36281f2914a37ed4d5a495fe1
Joe <josepha48@gmail.com>
2026-05-25 18:53:34 -0700

    switch to swc

--- onstep/config/webpack.js
+++ onstep/config/webpack.js
@@ -1,6 +1,89 @@
 import path from "path";
 
+const baseDir = process.cwd()import path from "path";
+
 const baseDir = process.cwd()
+
+const babelTargets = {
+    "edge": "120",
+    "chrome": "120",
+    "firefox": "120",
+    "safari": "15"
+};
+//const eslintConfig = fs.readFileSync( path.resolve( "./config/eslint.json" ) ).toString();
+
+/*const esJSON = JSON.parse( eslintConfig ),
+    esJSONWP = Object.keys( esJSON.globals );
+
+esJSON.globals = esJSONWP;*/
+let development = false;
+try {
+    development = JSON.parse(process.env['DEVELOPMENT']);
+} catch (_e) {}
+
+const CONFIG = {
+    "mode": "production",
+    "entry": {
+        "renderer": "./js/main"
+    },
+    context: path.resolve(baseDir),
+    output: {
+        "path": `${baseDir}/js`,
+        "filename": "renderer.js",
+        "chunkFilename": "renderer.bundle.js",
+        "sourceMapFilename": "renderer.source.map"
+    },
+    optimization: {
+        minimize: false
+    },
+    resolve: {
+        modules: [
+            "node_modules",
+            path.join(baseDir, "js")
+        ],
+        alias: {
+            'js': `${baseDir}/js`
+        },
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'swc-loader',
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: 'ecmascript', // Use 'ecmascript' for JS only
+                                jsx: true,
+                                dynamicImport: true
+                            },
+                            transform: {
+                                react: {
+                                    runtime: 'automatic', // Use modern JSX transform
+                                    refresh: true // Optional: Enable for Fast Refresh
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: []
+};
+
+if (development) {
+    CONFIG.devtool = 'source-map';
+}
+
+export default CONFIG;
+
 
 const babelTargets = {
     "edge": "120",
